4de9693b240ada84c9546aba92e6973059c4577d
makadown <maseflores79@gmail.com>
2020-06-20 11:28:24 -0500

    Creando modelo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    
+    AppRoutingModule,
     BrowserModule,
     FontAwesomeModule,
     SharedModule,
---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,7 @@
+export class Usuario {
+   /* id: number;
+    first_name: string;
+    last_name: string;
+    avatar: string;*/
+    constructor( id: string, first_name: string, last_name:string, avatar: string ) {}
+}
