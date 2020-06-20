d774601ad594fc19e3051d4d4f2d0603424336f3
makadown <maseflores79@gmail.com>
2020-06-20 10:56:46 -0500

    Creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    SharedModule,
+    UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
